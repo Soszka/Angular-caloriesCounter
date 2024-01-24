b3c046cae496005478dddf4f0234b70a574736c0
Soszka <bsocha1998@gmail.com>
2024-01-24 09:00:02 +0100

    basic styles created

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'Angular-caloriesCounter' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-caloriesCounter');
+    expect(app.title).toEqual('Licznik Kalorii');
   });
 
   it('should render title', () => {
