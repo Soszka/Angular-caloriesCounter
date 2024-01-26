175da199612394da27d79bf0fb0358eb2b422164
Soszka <bsocha1998@gmail.com>
2024-01-26 08:34:37 +0100

    mateiral module and font awesome initialized in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { HomeComponent } from './home/home.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { TitleComponent } from './title/title.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CalculatorComponent,
+    TitleComponent
   ],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    MaterialModule
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.scss'
+})
+export class CalculatorComponent {
+  sectionName = "KALKULATOR"
+  sectionDescription = "Wprowadź swoje dane i oblicz zapotrzebowanie kaloryczne"
+}
