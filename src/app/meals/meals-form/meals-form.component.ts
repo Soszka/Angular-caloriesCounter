ea27a643975607ed7ae9d1767bc3d2898bb52918
Soszka <bsocha1998@gmail.com>
2024-01-29 10:24:51 +0100

    meals basic structure created

---
+++ src/app/meals/meals-form/meals-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meals-form',
+  templateUrl: './meals-form.component.html',
+  styleUrl: './meals-form.component.scss'
+})
+export class MealsFormComponent {
+
+}
