834d7cfa7cbdcdaaa78ea0479d8c40d608443f53
Soszka <bsocha1998@gmail.com>
2024-02-01 13:15:14 +0100

    calories basic structure created

---
+++ src/app/meals/calories/calories-diagram/calories-diagram.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calories-diagram',
+  templateUrl: './calories-diagram.component.html',
+  styleUrl: './calories-diagram.component.scss'
+})
+export class CaloriesDiagramComponent {
+
+}
---
+++ src/app/meals/calories/calories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calories',
+  templateUrl: './calories.component.html',
+  styleUrl: './calories.component.scss'
+})
+export class CaloriesComponent {
+
+}
