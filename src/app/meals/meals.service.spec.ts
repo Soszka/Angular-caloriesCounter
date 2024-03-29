0fcf5bde04e1ca26ffe56cab78fda8b3b5d726f6
Bartlomiej Socha <batlomiej.socha@mtupolska.com>
2024-03-29 11:46:38 +0100

    tests added to meals module

---
+++ src/app/meals/meals.service.spec.ts
@@ -0,0 +1,61 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MealsService } from './meals.service';
+import { MealElement } from './meals.service';
+
+describe('MealsService', () => {
+  let service: MealsService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [MealsService]
+    });
+    service = TestBed.inject(MealsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('#fetchMeals should retrieve meals from the API', () => {
+    const testMeals = [
+      { name: 'Meal 1', calories: 100, carbohydrates: 20, protein: 10, fats: 5, kind: 'snack', taste: 'sweet' },
+      { name: 'Meal 2', calories: 200, carbohydrates: 40, protein: 20, fats: 10, kind: 'lunch', taste: 'salty' }
+    ];
+
+    service.fetchMeals().subscribe(meals => {
+      expect(meals.length).toBe(2);
+      expect(meals).toEqual(testMeals);
+    });
+
+    const req = httpTestingController.expectOne(service['mealsUrl']);
+    expect(req.request.method).toEqual('GET');
+    req.flush(testMeals);
+  });
+
+  it('#removeMeal should remove a meal and refresh the meals list', () => {
+  const initialMeals: MealElement[] = [
+    { name: 'Meal 1', calories: 100, carbohydrates: 20, protein: 10, fats: 5, kind: 'snack', taste: 'sweet' },
+    { name: 'Meal 2', calories: 200, carbohydrates: 40, protein: 20, fats: 10, kind: 'lunch', taste: 'salty' }
+  ];
+
+  service['originalElementDataSubject'].next(initialMeals);
+  service.removeMeal(initialMeals[0]).subscribe(() => {
+    service.elementData$.subscribe(meals => {
+      expect(meals.length).toBe(1);
+      expect(meals).not.toContain(initialMeals[0]);
+    });
+  });
+
+  const req = httpTestingController.expectOne(service['mealsUrl']);
+  expect(req.request.method).toEqual('PUT'); 
+  req.flush([initialMeals[1]]); 
+});
+});
