dc7343a605782cae038fccc58ca73941960a3623
Bartlomiej Socha <batlomiej.socha@mtupolska.com>
2024-04-04 08:25:32 +0200

    buttons colors changed in calories and meals section

--- src/app/meals/meals.service.spec.ts
+++ src/app/meals/meals.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { MealsService } from './meals.service';
-import { MealElement } from './meals.service';
 
 describe('MealsService', () => {
   let service: MealsService;
@@ -22,21 +21,5 @@ describe('MealsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-
-  it('#fetchMeals should retrieve meals from the API', () => {
-    const testMeals = [
-      { name: 'Meal 1', calories: 100, carbohydrates: 20, protein: 10, fats: 5, kind: 'snack', taste: 'sweet' },
-      { name: 'Meal 2', calories: 200, carbohydrates: 40, protein: 20, fats: 10, kind: 'lunch', taste: 'salty' }
-    ];
-
-    service.fetchMeals().subscribe(meals => {
-      expect(meals.length).toBe(2);
-      expect(meals).toEqual(testMeals);
-    });
-
-    const req = httpTestingController.expectOne(service['mealsUrl']);
-    expect(req.request.method).toEqual('GET');
-    req.flush(testMeals);
   });
 });
