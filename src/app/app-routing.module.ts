0fd6044cf2bbaf8a294f06ad5d83b664320ece65
Soszka <bsocha1998@gmail.com>
2024-11-23 11:24:43 +0100

    readme updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
-  { path: 'calories', loadChildren: () => import('./calories/calories.module').then(m => m.CaloriesModule) },
-  { path: 'meals', loadChildren: () => import('./meals/meals.module').then(m => m.MealsModule) },
-  { path: 'calculator', loadChildren: () => import('./calculator/calculator.module').then(m => m.CalculatorModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: '**', redirectTo: '' }
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'calories',
+    loadChildren: () =>
+      import('./calories/calories.module').then((m) => m.CaloriesModule),
+  },
+  {
+    path: 'meals',
+    loadChildren: () =>
+      import('./meals/meals.module').then((m) => m.MealsModule),
+  },
+  {
+    path: 'calculator',
+    loadChildren: () =>
+      import('./calculator/calculator.module').then((m) => m.CalculatorModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   appDescription: string = `Apliakcja ta ma za zadanie ułatwić Ci wyliczenie dziennego spożycia kalorycznego.
@@ -13,5 +13,5 @@ export class HomeComponent {
   formularz dzięki któremu możesz pobrać odpowiednie produkty z dokładnym opisem
   wyszczególnionym w tabeli. Każdy produkt możesz edytować a także dodawać do twojej
   dziennej listy spożytych produktów, którą znajdziesz w zakładce - KALORIE. Tam również znajduje
-  się diagram reprezentujacy twoje dzienne spożycie poszczególnych makroskładników. Zaczynajmy !`
+  się diagram reprezentujacy twoje dzienne spożycie poszczególnych makroskładników. Zaczynajmy !`;
 }
